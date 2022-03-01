2e255fc5719929c3591549c5de2fb39867cf2eed
Vinh <narutovinh@hotmail.fr>
2022-03-01 10:02:29 +0100

    fin exo 4

--- app/api/quizzes/questions/index.js
+++ app/api/quizzes/questions/index.js
@@ -6,16 +6,16 @@ const router = new Router({ mergeParams: true })
 
 router.get('/', (req, res) => {
   try {
-    res.status(200).json(Question.get())
+    res.status(200).json(Question.get().filter((e) => e.quizId === req.params.quizId))
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
-router.get('/:quizId', (req, res) => {
+router.get('/:questionId', (req, res) => {
   try {
-    console.log(req.params.quizId)
-    res.status(200).json(Question.getById(req.params.quizId))
+    console.log(req.params.questionId)
+    res.status(200).json(Question.getById(req.params.questionId).filter((e) => e.quizId === req.params.quizId))
   } catch (err) {
     res.status(500).json(err)
   }
@@ -23,7 +23,7 @@ router.get('/:quizId', (req, res) => {
 
 router.post('/', (req, res) => {
   try {
-    const question = Question.create({ ...req.body })
+    const question = Question.create({ ...req.body, quizId: parseInt(req.params.quizId, 10) })
     res.status(201).json(question)
   } catch (err) {
     if (err.name === 'ValidationError') {
@@ -34,19 +34,20 @@ router.post('/', (req, res) => {
   }
 })
 
-router.delete('/:quizId', (req, res) => {
+router.delete('/:questionId', (req, res) => {
   try {
-    console.log(req.params.quizId)
-    res.status(200).json(Question.delete(req.params.quizId))
+    const id = parseInt(req.params.questionId, 10)
+    const questionToDelete = Question.get().filter((q) => q.id === id).filter((e) => e.quizId === req.params.quizId)
+    Question.delete(id)
+    res.status(200).json(questionToDelete)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
-router.put('/:quizId', (req, res) => {
+router.put('/:questionId', (req, res) => {
   try {
-    console.log(req.body)
-    res.status(200).json(Question.update(req.body))
+    res.status(200).json(Question.update(req.params.questionId, req.body).filter(e => e.quizId === req.params.quizId))
   } catch (err) {
     res.status(500).json(err)
   }
